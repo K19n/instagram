19087ed6f9e417c248eb167ff2d1ba39be8fd338
keinun <yasumasa.kei@gmail.com>
2021-05-17 22:01:08 +0900

    コミットはじめ

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 安政　圭 on 2021/05/13.
+//
+
+import Foundation
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
+
+
