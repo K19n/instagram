de4801fc03c6b44df2425a4d90eada44ddd251e4
keinun <yasumasa.kei@gmail.com>
2021-05-25 23:15:24 +0900

    課題提出

--- Instagram/PostData.swift
+++ Instagram/PostData.swift
@@ -15,6 +15,7 @@ class PostData: NSObject {
     var date: Date?
     var likes: [String] = []
     var isLiked: Bool = false
+    var comment:[String] = []
 
     init(document: QueryDocumentSnapshot) {
         self.id = document.documentID
@@ -30,6 +31,9 @@ class PostData: NSObject {
 
         if let likes = postDic["likes"] as? [String] {
             self.likes = likes
+        }
+        if let comment = postDic["comment"] as? [String] {
+            self.comment = comment
         }
         if let myid = Auth.auth().currentUser?.uid {
             // likesの配列の中にmyidが含まれているかチェックすることで、自分がいいねを押しているかを判断
